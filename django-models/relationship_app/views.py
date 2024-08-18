48be4eb41af4c476663405bcff8b429d2a6b2409
Hazem Mohamed <hazem.mohaamed98@gmail.com>
2024-08-18 22:27:19 +0300

    Implementing Custom Permissions in Django

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -91,41 +91,31 @@ class MemberView(MemberRequiredMixin, TemplateView):
     template_name = 'member_view.html'
 
 # relationship_app/views.py
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 from .forms import BookForm
 
-@permission_required('relationship_app.can_add_book', raise_exception=True)
+@permission_required('relationship_app.can_add_book')
 def add_book(request):
     if request.method == 'POST':
         form = BookForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('book_list')
+            return redirect('list_books')
     else:
         form = BookForm()
     return render(request, 'add_book.html', {'form': form})
 
-@permission_required('relationship_app.can_change_book', raise_exception=True)
+@permission_required('relationship_app.can_change_book')
 def edit_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
         form = BookForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
-            return redirect('book_list')
+            return redirect('list_books')
     else:
         form = BookForm(instance=book)
     return render(request, 'edit_book.html', {'form': form})
-
-@permission_required('relationship_app.can_delete_book', raise_exception=True)
-def delete_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        book.delete()
-        return redirect('book_list')
-    return render(request, 'confirm_delete.html', {'book': book})
-
-
 
