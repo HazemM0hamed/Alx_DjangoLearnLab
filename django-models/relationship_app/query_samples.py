60571a272fcb10c8e1a332acb3b9fd8c1fedd244
Hazem Mohamed <hazem.mohaamed98@gmail.com>
2024-08-18 19:28:05 +0300

    Implement advanced model relationships in Django

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -2,7 +2,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    books = Book.objects.fliter(author=author)
+    books = Book.objects.filter(author=author)
     return books
 
 def list_books_in_library(library_name):
