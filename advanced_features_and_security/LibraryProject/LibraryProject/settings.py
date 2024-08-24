ee42b2824814350598afc9627f4ecbee68b72ac3
Hazem Mohamed <hazem.mohaamed98@gmail.com>
2024-08-24 21:24:11 +0300

    0. Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bookshelf',
 ]
-
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
