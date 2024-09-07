a4a784694cc401f1fe97a31490fe9c0bd5fe219f
Hazem Mohamed <hazem.mohaamed98@gmail.com>
2024-09-08 02:47:37 +0300

    Setting Up a New Django Project with Custom Serializers in Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,6 @@
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAdminOrReadOnly
 
 
@@ -9,5 +8,3 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAdminOrReadOnly]
-
-
