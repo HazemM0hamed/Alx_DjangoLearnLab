701503aef7770462b4593e4db807f17f2e30e998
Hazem Mohamed <hazem.mohaamed98@gmail.com>
2024-09-16 00:00:54 +0300

    Adding Comment Functionality to Blog Posts

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User, title, content,  published_date, author
+from django.contrib.auth.models import User
+from django.utils import timezone
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -17,3 +19,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, reverse
 from django.contrib import messages
-from .forms import UserRegisterForm, ProfileUpdateForm, PostForm
+from .forms import UserRegisterForm, ProfileUpdateForm, PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Post
+from .models import Post, Comment
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponseForbidden
 
 # Create your views here.
 def register(request):
@@ -75,3 +77,42 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+    
+@login_required
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return HttpResponseRedirect(reverse('post_detail', args=[post.id]))
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment.html', {'form': form, 'post': post})
+
+@login_required
+def edit_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+    if comment.author != request.user:
+        return HttpResponseForbidden()
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('post_detail', args=[comment.post.id]))
+    else:
+        form = CommentForm(instance=comment)
+    return render(request, 'blog/edit_comment.html', {'form': form, 'comment': comment})
+
+@login_required
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+    if comment.author != request.user:
+        return HttpResponseForbidden()
+    if request.method == 'POST':
+        comment.delete()
+        return HttpResponseRedirect(reverse('post_detail', args=[comment.post.id]))
+    return render(request, 'blog/delete_comment.html', {'comment': comment})
